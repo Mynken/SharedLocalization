043a175db0c7021a87bfa1169e4cd26cc4f38624
Vladyslav Hrehul <vladyslavherhul@biuro.k2.pl>
2019-04-05 11:21:34 +0200

    SharedLocalization.Common
    SharedLocalization.Common

---
+++ SharedLocalization/SharedLocalization.Common/DictionaryManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Resources;
+
+namespace SharedLocalization.Common
+{
+    public class DictionaryManager
+    {
+        public string Translate(string key)
+        {
+            ResourceManager rm = new ResourceManager("SharedLocalization.Common.Resources.Resource", typeof(DictionaryManager).GetTypeInfo().Assembly);
+
+            var currentCulture = CultureInfo.DefaultThreadCurrentCulture;
+            return rm.GetString(key, currentCulture);
+        }
+    }
+}
